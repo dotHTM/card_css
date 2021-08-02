a564baf1be2177150c9101b84da1629f75cbd1bd
Mike Cramer <imanerd@me.com>
2021-08-01 22:30:53 -0500

    Minimal Master

--- code/split.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# split.sh
-
-srcDir='../art files/exports'
-cardDir='../cards'
-
-mkdir -p "$cardDir"
-convert "$srcDir/card corners atlas.png" -crop 8x8@ +repage +adjoin "$cardDir/%d.png"
-
